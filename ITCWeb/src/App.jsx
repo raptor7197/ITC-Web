241b8a44a4684b894242ac8328c624f073c8a54d
raptor7197 <vamsikrishna75836@gmail.com>
2025-02-10 22:28:58 +0530

    .

--- ITCWeb/src/App.jsx
+++ ITCWeb/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'
 import Header from './Pages/Components/Header'
 import Footer from './Pages/Components/Footer'
 import Committee from './Pages/Committee'
+// import ConferenceCFP from './Pages/ConferenceCFP'
 
 function App() {
   
@@ -18,12 +17,10 @@ function App() {
       <Route path="/" element={<Home/>}/>
 
       <Route path="/Committee" element={<Committee/>}/>
-
-
       <Route path="/Committee" element={<Committee/>}/>
+      </Routes>
+    
 
-
-    </Routes>
      
      <Footer/>
     </>
