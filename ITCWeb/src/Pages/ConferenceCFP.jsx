9147ddcc20f229f26983e0662b03c37312748503
raptor7197 <vamsikrishna75836@gmail.com>
2025-02-11 19:18:05 +0530

    feat: ConferenceCFP page added

--- ITCWeb/src/Pages/ConferenceCFP.jsx
+++ ITCWeb/src/Pages/ConferenceCFP.jsx
@@ -2,9 +2,143 @@ import React from "react"
 
 function ConferenceCFP() {
   return (
-    <div className="">
-      
+    <>
+    <div className="bg-white text-center row-span-2">
+        <div className="">hello@##</div>
+        <div className=" bg-slate-300 ml-16 mr-16 rounded-xl">
+            {/* margin is here flex col to be applied  */}
+    <h1 className="text-blue-500 text-2xl font-serif font-bold pt-3">Call For Papers</h1> <br />
+    <h2 className="text-green-700 font-serif font-bold text-sm ">Call For Submissions</h2> <br />
+    <h1 className="text-blue-500 font-bold text-xl font-serif">Important Dates :</h1>
+    <h2 className="text-left font-semibold pl-7">Regular Papers : </h2>
+       <div className="text-left pl-8 ">
+        <li className="">
+        Paper submission dea-dline : 22 April, 2024  
+        </li>
+        <li>
+        Author notification : 2 June, 2024 
+        </li>
+        <li>
+         Final manuscript due : 30 June, 2024 
+        </li>
+       </div> <br />
+       <div className="text-left pl-7">
+            AUTHOR INSTRUCTIONS: Please check this <a href="" className="font-bold">document</a> for instructions of Final Paper Submissions. 
+        <br /><br />
+            Please note: Full length paper submission along with the abstract is allowed till submission deadline date.
+        <br /> <br />
+            Please note: We require the authors to follow this paper template: <a classname = "" href="">CFP template (CAMERA READY)</a>
+       </div>
+       <div className="text-left  font fw-semibold pl-3">Regular Papers : </div>
+       <h2 className="text-left font-semibold pl-3">Posters :  </h2>
+       <div className="text-left pl-2">
+        <li>
+         Poster submission deadline : 14 June, 2024 (Closed)
+        </li>
+        <li>
+        Author notification : 26 June, 2024 (Extended)
+        </li>
+        <li>
+         Final manuscript due : 7 July, 2024 (Extended)
+        </li>
+        </div> <br />
+        <button className="bg-slate-950 text-gray-50 p-2 rounded-bl-2xl rounded-tr-2xl">CFP template</button>
+        <h2 className="text-blue-500 font-bold pt-5">The Call For Papers for ITC Conference 2025 is live!</h2>
+        <p className="m-10 text-sm">International Test Conference is the world’s premier venue dedicated to the electronic test of devices, boards and systems—covering the complete cycle from design verification, design-for-test, design-for-manufacturing, silicon debug, manufacturing test, system test, diagnosis and failure analysis, and back to process and design improvement. At ITC India, design, test, and yield professionals can confront challenges faced by the industry, and learn how these challenges are being addressed by the combined efforts of academia, design tool and equipment suppliers, designers, and test engineers. This ITC India conference will be blend of virtual & direct participation focusing on Test development in India but the submissions may not be limited to topics related to this region. Topics related to design and test development across any geographical regions
+            will be of interest. </p>
+
+<p className="m-10 text-sm">Authors are invited to submit original, unpublished papers describing recent work in the field of test and design. In addition, authors are invited to submit high quality, practical, industry best practices. Submissions simultaneously under review or accepted by another conference, symposium or journal, will be summarily rejected. Accepted papers will be submitted for inclusion into IEEE Xplore subject to meeting IEEE Xplore’s scope and quality
+requirements.</p>
+<p className="m-10 text-sm">Authors are also invited to submit a single-page poster proposal, double-columned in Format (template available in EasyChair). Posters are a useful way of presenting late-breaking results, getting feedback on an innovative method, or participating without having to write a full paper.</p>
+<h2 className="text-blue-500 font-bold">Submission Guidelines</h2>
+<div className="text-left pl-2">
+<li>
+One or two topic(s) from the topic list, or a description of your topic. 
+</li><li>An abstract of 35 words or less to be entered online</li><li>An electronic copy of a complete paper up to 6 pages, double-columned in <a className = "font-bold" href="(https://www.ieee.org/conferences/publishing/templates.html)">IEEE Format, A4 size </a>
+ to be submitted through  <a className ="font-bold" href="(https://easychair.org/conferences/?conf=itcindia2024)">EasyChair</a>. Submissions less than 4 pages are rarely accepted.</li>
+ <li>Your submission must not include information that serves to identify
+the authors of the manuscript, such as name(s) or affiliation(s) of the author(s), anywhere in the manuscript, abstract, or in the embedded PDF data. References and bibliographic citations to the author(s) own published works or affiliations should be made in the third
+person</li></div>
+
+<div className="font-extralight font-serif italic">
+ITC India invites submissions on the latest advances in test, validation and diagnosis of ICs, boards and systems.</div><br />
+<h2 className="text-blue-500 font-bold">Topics of interest include (not limited to)</h2><br />
+<div className="flex justify-evenly pb-5">
+    <div className="bg-green-500 p-7 rounded-xl">
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+        <ul>3D/2.5D Test</ul>    
+
+    </div><br />
+    <div className="bg-green-500 p-7 rounded-xl">
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+        <ul>RF, mm-Wave and THz testing</ul>
+
+        </div><br />
+    <div className="bg-green-500 p-7 rounded-xl">
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+        <ul>Silicon Debug</ul>
+
+        </div><br />
+
+
+</div>
+
     </div>
+    {/* outside the container slate shade  */}
+    <div className="">hello
+
+    </div>
+    </div>
+
+
+    </>
   )
 }
 
