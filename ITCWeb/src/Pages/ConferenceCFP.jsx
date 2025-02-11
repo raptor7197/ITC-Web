e488b3d849dfc942e2897e2bb62e9d11ea6b2ff9
raptor7197 <vamsikrishna75836@gmail.com>
2025-02-11 22:28:40 +0530

    final CFP page

--- ITCWeb/src/Pages/ConferenceCFP.jsx
+++ ITCWeb/src/Pages/ConferenceCFP.jsx
@@ -3,15 +3,15 @@ import React from "react"
 function ConferenceCFP() {
   return (
     <>
-    <div className="bg-white text-center row-span-2">
-        <div className="">hello@##</div>
-        <div className=" bg-slate-300 ml-16 mr-16 rounded-xl">
+    <div className="bg-white text-center pb-3 flex">
+        
+        <div className=" bg-slate-300 ml-16  mt-5 rounded-xl w-4/5">
             {/* margin is here flex col to be applied  */}
     <h1 className="text-blue-500 text-2xl font-serif font-bold pt-3">Call For Papers</h1> <br />
     <h2 className="text-green-700 font-serif font-bold text-sm ">Call For Submissions</h2> <br />
     <h1 className="text-blue-500 font-bold text-xl font-serif">Important Dates :</h1>
-    <h2 className="text-left font-semibold pl-7">Regular Papers : </h2>
-       <div className="text-left pl-8 ">
+    <h2 className="text-left font-semibold pl-5">Regular Papers : </h2>
+       <div className="text-left pl-7 ">
         <li className="">
         Paper submission dea-dline : 22 April, 2024  
         </li>
@@ -22,16 +22,15 @@ function ConferenceCFP() {
          Final manuscript due : 30 June, 2024 
         </li>
        </div> <br />
-       <div className="text-left pl-7">
+       <div className="text-left text-sm pl-7">
             AUTHOR INSTRUCTIONS: Please check this <a href="" className="font-bold">document</a> for instructions of Final Paper Submissions. 
         <br /><br />
             Please note: Full length paper submission along with the abstract is allowed till submission deadline date.
         <br /> <br />
             Please note: We require the authors to follow this paper template: <a classname = "" href="">CFP template (CAMERA READY)</a>
-       </div>
-       <div className="text-left  font fw-semibold pl-3">Regular Papers : </div>
-       <h2 className="text-left font-semibold pl-3">Posters :  </h2>
-       <div className="text-left pl-2">
+       </div><br />
+       <h2 className="text-left font-semibold pl-7">Posters :  </h2>
+       <div className="text-left pl-7">
         <li>
          Poster submission deadline : 14 June, 2024 (Closed)
         </li>
@@ -42,7 +41,7 @@ function ConferenceCFP() {
          Final manuscript due : 7 July, 2024 (Extended)
         </li>
         </div> <br />
-        <button className="bg-slate-950 text-gray-50 p-2 rounded-bl-2xl rounded-tr-2xl">CFP template</button>
+        <button className="bg-gradient-to-tr from-yellow-400 to-green-400 text-gray-50 p-2 rounded-bl-2xl rounded-tr-2xl">CFP template</button>
         <h2 className="text-blue-500 font-bold pt-5">The Call For Papers for ITC Conference 2025 is live!</h2>
         <p className="m-10 text-sm">International Test Conference is the world’s premier venue dedicated to the electronic test of devices, boards and systems—covering the complete cycle from design verification, design-for-test, design-for-manufacturing, silicon debug, manufacturing test, system test, diagnosis and failure analysis, and back to process and design improvement. At ITC India, design, test, and yield professionals can confront challenges faced by the industry, and learn how these challenges are being addressed by the combined efforts of academia, design tool and equipment suppliers, designers, and test engineers. This ITC India conference will be blend of virtual & direct participation focusing on Test development in India but the submissions may not be limited to topics related to this region. Topics related to design and test development across any geographical regions
             will be of interest. </p>
@@ -50,7 +49,7 @@ function ConferenceCFP() {
 <p className="m-10 text-sm">Authors are invited to submit original, unpublished papers describing recent work in the field of test and design. In addition, authors are invited to submit high quality, practical, industry best practices. Submissions simultaneously under review or accepted by another conference, symposium or journal, will be summarily rejected. Accepted papers will be submitted for inclusion into IEEE Xplore subject to meeting IEEE Xplore’s scope and quality
 requirements.</p>
 <p className="m-10 text-sm">Authors are also invited to submit a single-page poster proposal, double-columned in Format (template available in EasyChair). Posters are a useful way of presenting late-breaking results, getting feedback on an innovative method, or participating without having to write a full paper.</p>
-<h2 className="text-blue-500 font-bold">Submission Guidelines</h2>
+<h2 className="text-blue-500 font-bold">Submission Guidelines</h2><br />
 <div className="text-left pl-2">
 <li>
 One or two topic(s) from the topic list, or a description of your topic. 
@@ -60,7 +59,7 @@ One or two topic(s) from the topic list, or a description of your topic.
 the authors of the manuscript, such as name(s) or affiliation(s) of the author(s), anywhere in the manuscript, abstract, or in the embedded PDF data. References and bibliographic citations to the author(s) own published works or affiliations should be made in the third
 person</li></div>
 
-<div className="font-extralight font-serif italic">
+<div className="font-extralight font-serif italic"><br />
 ITC India invites submissions on the latest advances in test, validation and diagnosis of ICs, boards and systems.</div><br />
 <h2 className="text-blue-500 font-bold">Topics of interest include (not limited to)</h2><br />
 <div className="flex justify-evenly pb-5">
@@ -124,17 +123,58 @@ ITC India invites submissions on the latest advances in test, validation and dia
         <ul>Silicon Debug</ul>
         <ul>Silicon Debug</ul>
         <ul>Silicon Debug</ul>
-
         </div><br />
 
 
 </div>
 
-    </div>
-    {/* outside the container slate shade  */}
-    <div className="">hello
 
     </div>
+    {/* outside the container slate shade  */}
+    <div>
+    <div className=" bg-gray-200  rounded-xl p-1 m-5 w-60">
+        <ul className="text-left m-3 text-sm ">
+            <h2 className="font-bold" >Conference</h2>
+            <li > → Latest Agenda</li>
+            <li>→ Tutorial Program</li>
+            <li>→ Keynote</li>
+            <li> → Panel Discussion</li>
+            <li> → Supportes 2025</li>
+            <li> → Best Paper & Honorable mentions (2025)</li>
+
+        </ul> 
+     </div>            
+            <div className=" bg-gray-200 rounded-xl m-5 p-1">
+                <ul className="text-left m-3 text-sm">
+                    <h2 className="font-bold">Submission</h2>
+                    <li>Call For Papers </li>
+                    <li>Call For Tutorials </li>
+                    <li>Academia Research Track (ART)</li>
+                    <li>Test Reality Check (TRC) </li>
+                    <li>Call for Fellowship </li>
+                </ul>
+            </div>
+            <div className= "bg-gray-200 rounded-xl p-1 m-5 hover:">
+                <ul className="text-left m-3 text-sm">
+                    <h2 className="font-bold">General Information</h2>
+                    <li>Comitee (2025)</li>
+                    <li>Contact Us </li>
+                </ul>
+            </div>
+            <div className= "bg-gray-200 rounded-xl p-1 m-5">
+                <ul className="text-left m-3 text-sm">
+                    <h2 className="font-bold">ITC Archives</h2>
+                    <li>ITC 2024</li>
+                    <li>ITC 2023</li>
+                    <li>ITC 2022</li>
+                    <li>ITC 2021</li>
+                    <li>ITC 2020</li>
+                    <li>ITC 2019</li>
+                    <li>ITC 2018</li>
+                    <li>ITC 2017</li>
+                </ul>
+            </div>
+        </div>
     </div>
 
 
